3704884e1bdb24cbb3a502748a05ca398da3beda
Bobo <Bobo@BOBO-DEV>
2023-01-09 15:50:40 +0100

    conf tests added

--- Database.Adapter.Infrastructure.Tests/Configurations/ConfigurationTests.cs
+++ Database.Adapter.Infrastructure.Tests/Configurations/ConfigurationTests.cs
@@ -1,21 +1,95 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Database.Adapter.Infrastructure.Configurations;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Database.Adapter.Infrastructure.Configurations;
+using Database.Adapter.Infrastructure.Exceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.Json;
+using static Database.Adapter.Infrastructure.Statics;
 
-namespace Database.Adapter.Infrastructure.Configurations.Tests;
+namespace Database.Adapter.Infrastructure.Tests.Configurations;
 
 [TestClass]
 public class ConfigurationTests
 {
+	private readonly Configuration configuration = new();
+	private readonly string configFilePath = Path.Combine(BaseDirectory, ConfigurationFileName);
+	private readonly JsonSerializerOptions jsonSerializerOptions = new() { WriteIndented = false };
+	private string configJson = string.Empty;
+
+	[TestInitialize]
+	public void TestInitialize()
+	{
+		configuration.Load();
+		configJson = JsonSerializer.Serialize(configuration, jsonSerializerOptions);
+	}
+
+	[TestCleanup]
+	public void TestCleanup()
+	{
+		File.WriteAllText(configFilePath, configJson);
+	}
+
 	[TestMethod]
 	public void GetConnectionStringSuccessTest()
 	{
 		Configuration configuration = new();
 		string connectionString = configuration.GetConnectionString("MasterContext");
 		Assert.IsFalse(string.IsNullOrWhiteSpace(connectionString));
+	}
+
+	[TestMethod]
+	public void LoadSuccessTest()
+	{
+		Configuration configuration = new();
+		configuration.Load();
+	}
+
+	[TestMethod]
+	public void LoadExceptionAlreadyLoadedTest()
+	{
+		Configuration configuration = new();
+		try
+		{
+			configuration.Load();
+			configuration.Load();
+		}
+		catch (Exception ex)
+		{
+			Assert.IsInstanceOfType(ex, typeof(ConfigurationException));
+		}
+	}
+
+	[TestMethod]
+	public void LoadExceptionFileFailedTest()
+	{
+		Configuration configuration = new();
+
+		if (File.Exists(configFilePath))
+			File.WriteAllText(configFilePath, "");
+
+		try
+		{
+			configuration.Load();
+		}
+		catch (Exception ex)
+		{
+			Assert.IsInstanceOfType(ex, typeof(ConfigurationException));
+		}
+	}
+
+	[TestMethod]
+	public void LoadExceptionFailedTest()
+	{
+		Configuration configuration = new();
+
+		if (File.Exists(configFilePath))
+			File.Delete(configFilePath);
+
+		try
+		{
+			configuration.Load();
+		}
+		catch (Exception ex)
+		{
+			Assert.IsInstanceOfType(ex, typeof(ConfigurationException));
+		}
 	}
 }
--- Database.Adapter.Infrastructure/Configurations/Configuration.cs
+++ Database.Adapter.Infrastructure/Configurations/Configuration.cs
@@ -1,5 +1,6 @@
 ﻿using Database.Adapter.Infrastructure.Exceptions;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using static Database.Adapter.Infrastructure.Properties.Resources;
 using static Database.Adapter.Infrastructure.Statics;
 
@@ -11,46 +12,18 @@ namespace Database.Adapter.Infrastructure.Configurations;
 /// <remarks>
 /// This strongly typed class is for the context and server configuration.
 /// </remarks>
+[Serializable]
 public sealed class Configuration
 {
+	[JsonIgnore]
 	private bool loaded;
 
 	/// <summary>The <see cref="SqlServers"/> property.</summary>
+	[JsonPropertyName(nameof(SqlServers))]
 	public List<SqlServer> SqlServers { get; set; } = new();
 	/// <summary>The <see cref="Contexts"/> property.</summary>
+	[JsonPropertyName(nameof(Contexts))]
 	public List<Context> Contexts { get; set; } = new();
-	/// <summary>
-	/// The context/database class.
-	/// </summary>
-	public class Context
-	{
-		/// <summary>The <see cref="Name"/> property.</summary>
-		public string Name { get; set; } = default!;
-		/// <summary>The <see cref="Database"/> property.</summary>
-		public string Database { get; set; } = default!;
-		/// <summary>The <see cref="IntegratedSecurity"/> property.</summary>
-		public bool IntegratedSecurity { get; set; } = default!;
-		/// <summary>The <see cref="MultipleActiveResultSets"/> property.</summary>
-		public bool MultipleActiveResultSets { get; set; } = default!;
-		/// <summary>The <see cref="PersistSecurityInfo"/> property.</summary>
-		public bool PersistSecurityInfo { get; set; } = default!;
-		/// <summary>The <see cref="UserName"/> property.</summary>
-		public string? UserName { get; set; } = default!;
-		/// <summary>The <see cref="Password"/> property.</summary>
-		public string? Password { get; set; } = default!;
-	}
-	/// <summary>
-	/// The sql server class.
-	/// </summary>
-	public class SqlServer
-	{
-		/// <summary>The <see cref="Name"/> property.</summary>
-		public string Name { get; set; } = default!;
-		/// <summary>The <see cref="Environment"/> property.</summary>
-		public string Environment { get; set; } = default!;
-		/// <summary>The <see cref="Server"/> property.</summary>
-		public string Server { get; set; } = default!;
-	}
 
 	/// <summary>
 	/// Should return the proper connection string for the provided database context.
@@ -170,4 +143,48 @@ public sealed class Configuration
 			throw new ConfigurationException(message, ex);
 		}
 	}
+}
+
+/// <summary>
+/// The context/database class.
+/// </summary>
+public sealed class Context
+{
+	/// <summary>The <see cref="Name"/> property.</summary>
+	[JsonPropertyName(nameof(Name))]
+	public string Name { get; set; } = default!;
+	/// <summary>The <see cref="Database"/> property.</summary>
+	[JsonPropertyName(nameof(Database))]
+	public string Database { get; set; } = default!;
+	/// <summary>The <see cref="IntegratedSecurity"/> property.</summary>
+	[JsonPropertyName(nameof(IntegratedSecurity))]
+	public bool IntegratedSecurity { get; set; } = default!;
+	/// <summary>The <see cref="MultipleActiveResultSets"/> property.</summary>
+	[JsonPropertyName(nameof(MultipleActiveResultSets))]
+	public bool MultipleActiveResultSets { get; set; } = default!;
+	/// <summary>The <see cref="PersistSecurityInfo"/> property.</summary>
+	[JsonPropertyName(nameof(PersistSecurityInfo))]
+	public bool PersistSecurityInfo { get; set; } = default!;
+	/// <summary>The <see cref="UserName"/> property.</summary>
+	[JsonPropertyName(nameof(UserName))]
+	public string? UserName { get; set; } = default!;
+	/// <summary>The <see cref="Password"/> property.</summary>
+	[JsonPropertyName(nameof(Password))]
+	public string? Password { get; set; } = default!;
+}
+
+/// <summary>
+/// The sql server class.
+/// </summary>
+public sealed class SqlServer
+{
+	/// <summary>The <see cref="Name"/> property.</summary>
+	[JsonPropertyName(nameof(Name))]
+	public string Name { get; set; } = default!;
+	/// <summary>The <see cref="Environment"/> property.</summary>
+	[JsonPropertyName(nameof(Environment))]
+	public string Environment { get; set; } = default!;
+	/// <summary>The <see cref="Server"/> property.</summary>
+	[JsonPropertyName(nameof(Server))]
+	public string Server { get; set; } = default!;
 }

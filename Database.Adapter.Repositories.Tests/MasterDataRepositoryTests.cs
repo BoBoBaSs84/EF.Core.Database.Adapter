91f488928a6b4399abb13e3dbf5530adf1f1b01a
Bobo <Bobo@BOBO-DEV>
2023-01-09 16:13:24 +0100

    a start ...

---
+++ Database.Adapter.Repositories.Tests/MasterDataRepositoryTests.cs
@@ -0,0 +1,21 @@
+﻿using Database.Adapter.Entities.MasterData;
+using Database.Adapter.Repositories.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Database.Adapter.Repositories.Tests;
+
+[TestClass]
+public class MasterDataRepositoryTests
+{
+	private IMasterDataRepository masterDataRepository;
+
+	[TestInitialize]
+	public void TestInitialize() => masterDataRepository = new MasterDataRepository();
+
+	[TestMethod]
+	public void DayTypeRepositoryGetAllSuccessTest()
+	{
+		IQueryable<DayType> test = masterDataRepository.DayTypeRepository.GetAll();
+		Assert.IsNotNull(test);
+	}
+}

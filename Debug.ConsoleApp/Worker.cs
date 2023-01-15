b1ad9a437a206595d7cad5659cb5a1f06a434afc
Bobo <Bobo@BOBO-DEV>
2023-01-15 22:16:37 +0100

    fix

--- Debug.ConsoleApp/Worker.cs
+++ Debug.ConsoleApp/Worker.cs
@@ -26,11 +26,6 @@ public class Worker : BackgroundService
 	{
 		while (!stoppingToken.IsCancellationRequested)
 		{
-			var user = authenticationRepository.UserRepository.GetByEmail("Test");
-
-
-
-
 			List<CalendarDay> calendarDaysByYear = masterDataRepository.CalendarRepository.GetManyByCondition(
 				expression: x => x.Year.Equals(2023),
 				includeProperties: new[] { nameof(CalendarDay.DayType) }

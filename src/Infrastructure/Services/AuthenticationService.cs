565bde7498ae9c7dfb6d426b5d34c32b07ea4b59
Bobo <Bobo@BOBO-DEV>
2023-03-26 10:49:09 +0200

    Changes: - Claims

--- src/Infrastructure/Services/AuthenticationService.cs
+++ src/Infrastructure/Services/AuthenticationService.cs
@@ -290,10 +290,8 @@ internal sealed class AuthenticationService : IAuthenticationService
 	{
 		IList<Claim> claims = new List<Claim>()
 		{
-			new(ClaimTypes.Email, user.Email),
 			new(ClaimTypes.Name, user.UserName),
 			new(ClaimTypes.NameIdentifier, $"{user.Id}"),
-			new(ClaimTypes.DateOfBirth, $"{user.DateOfBirth}")
 		};
 
 		IList<string> roles = await _userService.GetRolesAsync(user);

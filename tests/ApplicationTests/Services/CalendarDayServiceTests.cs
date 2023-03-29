6e9b146ab7d1ee63efa5da32b3d3322afd1dcb1a
Bobo <Bobo@BOBO-DEV>
2023-03-29 18:41:14 +0200

    init commit

---
+++ tests/ApplicationTests/Services/CalendarDayServiceTests.cs
@@ -0,0 +1,29 @@
+﻿namespace ApplicationTests.Services;
+
+[TestClass()]
+public class CalendarDayServiceTests
+{
+	[TestMethod()]
+	public void GetByDateTest()
+	{
+
+	}
+
+	[TestMethod()]
+	public void GetByIdTest()
+	{
+
+	}
+
+	[TestMethod()]
+	public void GetPagedByParametersTest()
+	{
+
+	}
+
+	[TestMethod()]
+	public void GetCurrentDateTest()
+	{
+
+	}
+}

20e1f956dd0e69273d2ecee2a6289b21c5489b36
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2023-09-10 11:46:58 +0200

    feat: load calendar entries by date part
    changes:
    - extension methods added
    - service methods changed
    - filter methods removed
    - parameters removed
    
    closes #209

--- src/Domain/Extensions/DateTimeExtension.cs
+++ src/Domain/Extensions/DateTimeExtension.cs
@@ -12,16 +12,16 @@ public static class DateTimeExtension
 	/// </summary>
 	/// <param name="dateTime">The date time to modify.</param>
 	/// <returns><see cref="DateTime"/></returns>
-	public static DateTime ToSqlDate(this DateTime dateTime) =>
-		DateTime.Parse(dateTime.ToShortDateString(), CultureInfo.CurrentCulture);
+	public static DateTime ToSqlDate(this DateTime dateTime)
+		=> DateTime.Parse(dateTime.ToShortDateString(), CultureInfo.InvariantCulture);
 
 	/// <summary>
 	/// Should cut of the time in date time.
 	/// </summary>
 	/// <param name="dateTime">The date time to modify.</param>
 	/// <returns><see cref="DateTime"/></returns>
-	public static DateTime? ToSqlDate(this DateTime? dateTime) =>
-		!dateTime.HasValue ? null : DateTime.Parse(dateTime.Value.ToShortDateString(), CultureInfo.CurrentCulture);
+	public static DateTime? ToSqlDate(this DateTime? dateTime)
+		=> !dateTime.HasValue ? null : DateTime.Parse(dateTime.Value.ToShortDateString(), CultureInfo.InvariantCulture);
 
 	/// <summary>
 	/// Calculates the first day of the week using the specified date and
@@ -42,4 +42,20 @@ public static class DateTimeExtension
 	/// <returns>The date of the last day of the week.</returns>
 	public static DateTime EndOfWeek(this DateTime dateTime, DayOfWeek startOfWeek = DayOfWeek.Monday)
 		=> StartOfWeek(dateTime, startOfWeek).AddDays(6);
+
+	/// <summary>
+	/// Calculates the first day of the month using the specified date.
+	/// </summary>
+	/// <param name="dateTime">The date time value to work with.</param>
+	/// <returns>The date of the first day of the month.</returns>
+	public static DateTime StartOfMonth(this DateTime dateTime)
+		=> new(dateTime.Year, dateTime.Month, 1);
+
+	/// <summary>
+	/// Calculates the last day of the month using the specified date.
+	/// </summary>
+	/// <param name="dateTime">The date time value to work with.</param>
+	/// <returns>The date of the last day of the month.</returns>
+	public static DateTime EndOfMonth(this DateTime dateTime)
+		=> StartOfMonth(dateTime).AddMonths(1).AddDays(-1);
 }
--- src/Domain/Extensions/XmlExtension.cs
+++ src/Domain/Extensions/XmlExtension.cs
@@ -1,6 +1,7 @@
-﻿using System.Text;
-using System.Xml;
+﻿using System.Xml;
 using System.Xml.Serialization;
+
+using Domain.Helpers;
 
 namespace Domain.Extensions;
 
@@ -69,29 +70,5 @@ public static class XmlExtension
 		using XmlReader xmlReader = XmlReader.Create(stringReader, settings);
 		XmlSerializer serializer = new(value.GetType());
 		return (T)serializer.Deserialize(xmlReader)!;
-	}
-
-	/// <summary>
-	/// Overrides the base 'StringWriter' class to accept a different character encoding type.
-	/// </summary>
-	private class StringWriterWithEncoding : StringWriter
-	{
-		private readonly Encoding _encoding = default!;
-
-		/// <summary>
-		/// Overrides the default encoding type (UTF-16).
-		/// </summary>
-		public override Encoding Encoding => _encoding ?? base.Encoding;
-
-		/// <summary>
-		/// Initializes a new instance of the <see cref="StringWriterWithEncoding"/> class.
-		/// </summary>
-		public StringWriterWithEncoding() { }
-
-		/// <summary>
-		/// Initializes a new instance of the <see cref="StringWriterWithEncoding"/> class.
-		/// </summary>
-		/// <param name="encoding">The character encoding type</param>
-		public StringWriterWithEncoding(Encoding encoding) => _encoding = encoding;
 	}
 }

f6073ef7f3d047de09c2fba36a44583d76be6b16
Robert Peter Meyer <robert.rm.meyer@siemens.com>
2023-09-11 13:39:46 +0200

    feat: flags to string array converter
    changes:
    - converter added
    - unit tests added
    
    closes #213

---
+++ tests/DomainTests/Converters/JsonConverters/FlagsToArrayConverterTests.cs
@@ -0,0 +1,93 @@
+﻿using System.Text.Json.Serialization;
+
+using Domain.Converters.JsonConverters;
+using Domain.Extensions;
+
+using FluentAssertions;
+
+namespace DomainTests.Converters.JsonConverters;
+
+[TestClass]
+public class FlagsToArrayConverterTests
+{
+	[TestMethod]
+	public void ReadNoValueTest()
+	{
+		string jsonString = "{}";
+
+		TestClass testClass = new TestClass().FromJsonString(jsonString);
+
+		testClass.Flag.Should().HaveFlag(TestFlag.None);
+	}
+
+	[TestMethod]
+	public void ReadValueTest()
+	{
+		string jsonString = @"{""flag"":[""Complex""]}";
+
+		TestClass testClass = new TestClass().FromJsonString(jsonString);
+
+		testClass.Flag.Should().HaveFlag(TestFlag.Complex);
+	}
+
+	[TestMethod]
+	public void ReadValuesTest()
+	{
+		string jsonString = @"{""flag"":[""Simple"",""Medium""]}";
+
+		TestClass testClass = new TestClass().FromJsonString(jsonString);
+
+		testClass.Flag.Should().HaveFlag(TestFlag.Simple | TestFlag.Medium);
+	}
+
+	[TestMethod]
+	public void WriteNoValueTest()
+	{
+		TestClass testClass = new();
+
+		string jsonString = testClass.ToJsonString();
+
+		jsonString.Should().Be(@"{""flag"":[""None""]}");
+	}
+
+	[TestMethod]
+	public void WriteValueTest()
+	{
+		TestClass testClass = new(TestFlag.Simple);
+
+		string jsonString = testClass.ToJsonString();
+
+		jsonString.Should().Be(@"{""flag"":[""Simple""]}");
+	}
+
+	[TestMethod]
+	public void WriteValuesTest()
+	{
+		TestClass testClass = new(TestFlag.Simple | TestFlag.Medium);
+
+		string jsonString = testClass.ToJsonString();
+
+		jsonString.Should().Be(@"{""flag"":[""Simple"",""Medium""]}");
+	}
+
+	private class TestClass
+	{
+		public TestClass()
+		{ }
+
+		public TestClass(TestFlag flag)
+			=> Flag = flag;
+
+		[JsonConverter(typeof(FlagsToArrayJsonConverter))]
+		public TestFlag Flag { get; set; }
+	}
+
+	[Flags]
+	private enum TestFlag
+	{
+		None = 0,
+		Simple = 1 << 0,
+		Medium = 1 << 1,
+		Complex = 1 << 2,
+	}
+}
---
+++ tests/DomainTests/Converters/JsonConverters/TimeSpanJsonConverterTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Text.Json.Serialization;
+
+using Domain.Converters.JsonConverters;
+using Domain.Extensions;
+
+using FluentAssertions;
+
+namespace DomainTests.Converters.JsonConverters;
+
+[TestClass]
+public class TimeSpanJsonConverterTests : DomainTestBase
+{
+	private readonly TimeSpan _time = new(6, 0, 0);
+
+	[TestMethod]
+	public void ReadValueTest()
+	{
+		string jsonString = @"{""time"":""06:00:00""}";
+
+		TestClass testClass = new TestClass().FromJsonString(jsonString);
+
+		testClass.Time.Should().Be(_time);
+	}
+
+	[TestMethod]
+	public void ReadNoValueTest()
+	{
+		string jsonString = "{}";
+
+		TestClass testClass = new TestClass().FromJsonString(jsonString);
+
+		testClass.Time.Should().BeNull();
+	}
+
+	[TestMethod]
+	public void WriteValueTest()
+	{
+		TestClass testClass = new(_time);
+
+		string jsonString = testClass.ToJsonString();
+
+		jsonString.Should().Be(@"{""time"":""06:00:00""}");
+	}
+
+	[TestMethod]
+	public void WriteNoValueTest()
+	{
+		TestClass testClass = new(null);
+
+		string jsonString = testClass.ToJsonString();
+
+		jsonString.Should().Be("{}");
+	}
+
+	private class TestClass
+	{
+		public TestClass()
+		{ }
+
+		public TestClass(TimeSpan? time)
+			=> Time = time;
+
+		[JsonConverter(typeof(TimeSpanJsonConverter))]
+		public TimeSpan? Time { get; set; }
+	}
+}

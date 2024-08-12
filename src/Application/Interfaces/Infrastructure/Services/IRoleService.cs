68ed611ff015f63bb523b5c1189ae875c1d44bb6
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-08-12 09:21:39 +0200

    feat: added role and user service interfaces

---
+++ src/Application/Interfaces/Infrastructure/Services/IRoleService.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models.Identity;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.Interfaces.Infrastructure.Services;
+
+/// <summary>
+/// The interface for the role service.
+/// </summary>
+public interface IRoleService
+{
+	/// <inheritdoc cref="RoleManager{TRole}.FindByIdAsync(string)"/>
+	Task<RoleModel?> FindByIdAsync(string roleId);
+}

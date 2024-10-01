755300f823a8e738a61358524284528a14f6fc53
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-10-01 20:03:19 +0200

    feat: fluent validation and validators added

---
+++ src/Application/Validators/Identity/AuthenticationRequestValidator.cs
@@ -0,0 +1,21 @@
+﻿using Application.Contracts.Requests.Identity;
+
+using FluentValidation;
+
+namespace Application.Validators.Identity;
+
+/// <summary>
+/// The validator for the authentication request.
+/// </summary>
+[SuppressMessage("Style", "IDE0058", Justification = "Not relevant here, fluent validation.")]
+public sealed class AuthenticationRequestValidator : AbstractValidator<AuthenticationRequest>
+{
+	/// <summary>
+	/// Initializes an instance of <see cref="AuthenticationRequestValidator"/> class.
+	/// </summary>
+	public AuthenticationRequestValidator()
+	{
+		RuleFor(x => x.UserName).NotEmpty();
+		RuleFor(x => x.Password).NotEmpty();
+	}
+}

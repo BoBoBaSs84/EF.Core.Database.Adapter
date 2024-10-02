93612635b3c42b037325765ce0700f9260620af9
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-10-02 07:39:35 +0200

    feat: custom validator and extension method added

---
+++ src/Application/Extensions/ValidatorExtensions.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+
+using static Application.Validators.CustomValidators;
+
+namespace Application.Extensions;
+
+/// <summary>
+/// The <see cref="PropertyValidator{T, TProperty}"/> extension class.
+/// </summary>
+public static class ValidatorExtensions
+{
+	/// <summary>
+	/// Checks if the property is a valid phone number.
+	/// </summary>
+	/// <typeparam name="T">The type to work with.</typeparam>
+	/// <param name="ruleBuilder">The rule binder to extend.</param>
+	public static IRuleBuilderOptions<T, string?> Phone<T>(this IRuleBuilder<T, string?> ruleBuilder)
+		=> ruleBuilder.SetValidator(new PhoneValidator<T>());
+}
---
+++ src/Application/Validators/CustomValidators.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using FluentValidation;
+using FluentValidation.Validators;
+
+namespace Application.Validators;
+
+/// <summary>
+/// The static class for custom validators.
+/// </summary>
+public static class CustomValidators
+{
+	/// <summary>
+	/// The phone custom validator class.
+	/// </summary>
+	/// <typeparam name="T">The type to work with.</typeparam>
+	public sealed class PhoneValidator<T> : PropertyValidator<T, string?>
+	{
+		private static readonly PhoneAttribute PhoneAttribute = new();
+
+		/// <inheritdoc/>
+		public override string Name => "PhoneValidator";
+
+		/// <inheritdoc/>
+		public override bool IsValid(ValidationContext<T> context, string? value)
+			=> value is null || PhoneAttribute.IsValid(value);
+
+		/// <inheritdoc/>
+		protected override string GetDefaultMessageTemplate(string errorCode)
+			=> "'{PropertyName}' is not a valid phone number.";
+	}
+}

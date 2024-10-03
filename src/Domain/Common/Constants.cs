4416c0215832a4791760903ae3c82e93d4922fd9
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-10-03 10:26:39 +0200

    feat: bic regex added

--- src/Domain/Common/Constants.cs
+++ src/Domain/Common/Constants.cs
@@ -236,7 +236,7 @@ public static class Constants
 		/// <summary>
 		/// The International Bank Account Number regex pattern.
 		/// </summary>
-		public const string IBAN = @"^[A-Z]{2}(?:[ ]?[0-9]){18,20}$";
+		public const string IBAN = @"^[a-zA-Z]{2}[0-9]{2}([a-zA-Z0-9]?){16,30}$";
 
 		/// <summary>
 		/// A color is represented by three consecutive hexadecimal numbers, each of which represents a color in the RGB color space.
@@ -250,5 +250,10 @@ public static class Constants
 		/// Matches Visa, MasterCard, American Express, Diners Club, Discover and JCB cards.
 		/// </remarks>
 		public const string PAN = @"^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$";
+
+		/// <summary>
+		/// The bank identification code regex pattern.
+		/// </summary>
+		public const string BIC = @"^[A-Z0-9]{4}[A-Z]{2}[A-Z0-9]{2}(?:[A-Z0-9]{3})?$";
 	}
 }
--- src/Domain/Common/RegexStatics.cs
+++ src/Domain/Common/RegexStatics.cs
@@ -15,13 +15,21 @@ public static partial class RegexStatics
 	public static Regex PAN { get; } = PANRegex();
 
 	/// <summary>
-	/// The International Bank Account Number regex.
+	/// The international bank account number regex.
 	/// </summary>
 	public static Regex IBAN { get; } = IBANRegex();
+
+	/// <summary>
+	/// The bank identification code regex.
+	/// </summary>
+	public static Regex BIC { get; } = BICRegex();
 
 	[GeneratedRegex(RegexPatterns.PAN)]
 	private static partial Regex PANRegex();
 
 	[GeneratedRegex(RegexPatterns.IBAN)]
 	private static partial Regex IBANRegex();
+
+	[GeneratedRegex(RegexPatterns.BIC)]
+	private static partial Regex BICRegex();
 }

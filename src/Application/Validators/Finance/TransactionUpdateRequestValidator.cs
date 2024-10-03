ff57bc93cbcf92a8aab0c96694584982ac64c1c9
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-10-03 10:27:45 +0200

    feat: transaction request validators added

---
+++ src/Application/Validators/Finance/TransactionUpdateRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using Application.Contracts.Requests.Finance;
+using Application.Validators.Finance.Base;
+
+using FluentValidation;
+
+namespace Application.Validators.Finance;
+
+/// <summary>
+/// The validator for the transaction update request.
+/// </summary>
+public sealed class TransactionUpdateRequestValidator : AbstractValidator<TransactionUpdateRequest>
+{
+	/// <summary>
+	/// Initializes an instance of <see cref="TransactionUpdateRequestValidator"/> class.
+	/// </summary>
+	public TransactionUpdateRequestValidator()
+		=> Include(new TransactionBaseRequestValidator());
+}

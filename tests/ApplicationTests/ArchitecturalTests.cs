8cc4b7291b23bd3b57f8d5203f2d069be78c0b34
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-10-06 10:26:29 +0200

    feat: finance request unit tests added

---
+++ tests/ApplicationTests/ArchitecturalTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection;
+
+using Application.Common;
+
+using BaseTests.Helpers;
+
+using BB84.Extensions;
+
+using FluentAssertions;
+
+namespace ApplicationTests;
+
+[TestClass]
+[SuppressMessage("Style", "IDE0058", Justification = "Not relevant here, unit testing.")]
+public sealed class ArchitecturalTests : ApplicationTestBase
+{
+	private readonly Assembly _assembly = typeof(IApplicationAssemblyMarker).Assembly;
+
+	[TestMethod]
+	public void ServicesShouldNotBePublicAndShouldBeSealed()
+	{
+		IEnumerable<Type> typeList = TypeHelper.GetAssemblyTypes(
+			assembly: _assembly,
+			expression: x => x.Name.EndsWith("Service", StringComparison.OrdinalIgnoreCase) && x.IsInterface.Equals(false)
+			);
+
+		AssertionHelper.AssertInScope(() =>
+		{
+			typeList.Should().NotBeNullOrEmpty();
+			typeList.ForEach(t =>
+			{
+				t.IsSealed.Should().BeTrue();
+				t.IsPublic.Should().BeFalse();
+				t.IsVisible.Should().BeFalse();
+			});
+		});
+	}
+
+	[TestMethod]
+	public void ValidatorsShouldBePublicAndShouldBeSealed()
+	{
+		IEnumerable<Type> typeList = TypeHelper.GetAssemblyTypes(
+			assembly: _assembly,
+			expression: x => x.Name.EndsWith("Validator", StringComparison.OrdinalIgnoreCase) && x.IsInterface.Equals(false)
+			);
+
+		AssertionHelper.AssertInScope(() =>
+		{
+			typeList.Should().NotBeNullOrEmpty();
+			typeList.ForEach((t) =>
+			{
+				t.IsSealed.Should().BeTrue();
+				t.IsPublic.Should().BeTrue();
+			});
+		});
+	}
+}
---
+++ tests/ApplicationTests/Validators/Contracts/Finance/AccountCreateRequestValidatorTests.cs
@@ -0,0 +1,62 @@
+﻿using Application.Contracts.Requests.Finance;
+
+using ApplicationTests.Helpers;
+
+using BaseTests.Constants;
+using BaseTests.Helpers;
+
+using Domain.Enumerators.Finance;
+
+using FluentAssertions;
+
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace ApplicationTests.Validators.Contracts.Finance;
+
+[TestClass]
+[SuppressMessage("Style", "IDE0058", Justification = "Not relevant here, fluent assertions.")]
+public sealed class AccountCreateRequestValidatorTests : ApplicationTestBase
+{
+	private IValidator<AccountCreateRequest>? _validator;
+
+	[TestMethod]
+	public void RequestShouldBeValidWhenPropertiesAreCorrect()
+	{
+		_validator = CreateValidatorInstance();
+		AccountCreateRequest request = RequestHelper.GetAccountCreateRequest();
+
+		ValidationResult result = _validator.Validate(request);
+
+		AssertionHelper.AssertInScope(() =>
+		{
+			result.Should().NotBeNull();
+			result.IsValid.Should().BeTrue();
+			result.Errors.Should().BeEmpty();
+		});
+	}
+
+	[TestMethod]
+	public void RequestShouldNotBeValidWhenPropertiesAreNotCorrect()
+	{
+		_validator = CreateValidatorInstance();
+		AccountCreateRequest request = new()
+		{
+			IBAN = RandomHelper.GetString(20, TestConstants.WildCardChars),
+			Provider = RandomHelper.GetString(501),
+			Type = (AccountType)42
+		};
+
+		ValidationResult result = _validator.Validate(request);
+
+		result.Should().NotBeNull();
+		result.IsValid.Should().BeFalse();
+		result.Errors.Should().HaveCount(3);
+		result.Errors.Should().Contain(x => x.PropertyName == nameof(request.IBAN));
+		result.Errors.Should().Contain(x => x.PropertyName == nameof(request.Provider));
+		result.Errors.Should().Contain(x => x.PropertyName == nameof(request.Type));
+	}
+
+	private static IValidator<AccountCreateRequest> CreateValidatorInstance()
+		=> GetService<IValidator<AccountCreateRequest>>();
+}

c0f6bad546e1aa7fb80aaaa0bc26835dfbcce3cc
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-10-08 17:59:11 +0200

    feat: migration applied

--- src/Infrastructure/Persistence/Migrations/20241008155707_TodoListColumnUpdate.Designer.cs
+++ src/Infrastructure/Persistence/Migrations/20241008155707_TodoListColumnUpdate.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(RepositoryContext))]
-    partial class RepositoryContextModelSnapshot : ModelSnapshot
+    [Migration("20241008155707_TodoListColumnUpdate")]
+    partial class TodoListColumnUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

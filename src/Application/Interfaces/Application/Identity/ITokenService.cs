b74cf675f126b5fe4e33e499b6067aecc369f821
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-10-19 10:02:49 +0200

    feat: service interfaces modified

---
+++ src/Application/Interfaces/Application/Identity/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace Application.Interfaces.Application.Identity;
+
+/// <summary>
+/// The interface for the token service.
+/// </summary>
+public interface ITokenService
+{
+	string GenerateAccessToken(IEnumerable<Claim> claims);
+	ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
+}

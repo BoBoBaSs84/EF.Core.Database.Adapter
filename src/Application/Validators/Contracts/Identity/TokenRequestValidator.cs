5cf0a6fee0eb1b0f2fbca61f278a2425aacb85db
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-10-19 19:36:17 +0200

    feat: token request validator and unit tests added

---
+++ src/Application/Validators/Contracts/Identity/TokenRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using Application.Contracts.Requests.Identity;
+
+using FluentValidation;
+
+namespace Application.Validators.Contracts.Identity;
+
+/// <summary>
+/// The validator for the token request.
+/// </summary>
+[SuppressMessage("Style", "IDE0058", Justification = "Not relevant here, fluent validation.")]
+public sealed class TokenRequestValidator : AbstractValidator<TokenRequest>
+{
+	/// <summary>
+	/// Initializes an instance of <see cref="AuthenticationRequestValidator"/> class.
+	/// </summary>
+	public TokenRequestValidator()
+	{
+		RuleFor(x => x.AccessToken)
+			.NotEmpty();
+
+		RuleFor(x => x.RefreshToken)
+			.NotEmpty();
+	}
+}
---
+++ tests/ApplicationTests/Validators/Contracts/Identity/TokenRequestValidatorTests.cs
@@ -0,0 +1,58 @@
+﻿using Application.Contracts.Requests.Identity;
+
+using FluentAssertions;
+
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace ApplicationTests.Validators.Contracts.Identity;
+
+[TestClass()]
+[SuppressMessage("Style", "IDE0058", Justification = "Not relevant here, fluent assertions.")]
+public sealed class TokenRequestValidatorTests : ApplicationTestBase
+{
+	private IValidator<TokenRequest> _validator = default!;
+
+	[DataTestMethod]
+	[DataRow(""), DataRow(null)]
+	public void AccessTokenShouldNotBeEmpty(string accessToken)
+	{
+		_validator = CreateValidatorInstance();
+		TokenRequest request = new()
+		{
+			AccessToken = accessToken,
+			RefreshToken = nameof(TokenRequest.RefreshToken)
+		};
+
+		ValidationResult result = _validator.Validate(request);
+
+		result.Should().NotBeNull();
+		result.IsValid.Should().BeFalse();
+		result.Errors.Should().NotBeEmpty();
+		result.Errors.Should().HaveCount(1);
+		result.Errors.Should().Contain(x => x.PropertyName == nameof(request.AccessToken));
+	}
+
+	[DataTestMethod]
+	[DataRow(""), DataRow(null)]
+	public void RefreshTokenShouldNotBeEmpty(string refreshToken)
+	{
+		_validator = CreateValidatorInstance();
+		TokenRequest request = new()
+		{
+			AccessToken = nameof(TokenRequest.AccessToken),
+			RefreshToken = refreshToken
+		};
+
+		ValidationResult result = _validator.Validate(request);
+
+		result.Should().NotBeNull();
+		result.IsValid.Should().BeFalse();
+		result.Errors.Should().NotBeEmpty();
+		result.Errors.Should().HaveCount(1);
+		result.Errors.Should().Contain(x => x.PropertyName == nameof(request.AccessToken));
+	}
+
+	private static IValidator<TokenRequest> CreateValidatorInstance()
+		=> GetService<IValidator<TokenRequest>>();
+}

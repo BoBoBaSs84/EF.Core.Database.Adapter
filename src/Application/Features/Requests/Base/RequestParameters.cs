3c690473a376367b2814e812e4192d71d4a8b851
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-10-20 09:59:14 +0200

    fix: request parameters fix applied

--- src/Application/Features/Requests/Base/RequestParameters.cs
+++ src/Application/Features/Requests/Base/RequestParameters.cs
@@ -5,14 +5,19 @@
 /// </summary>
 public abstract class RequestParameters
 {
+	private const int MinPageNumber = 1;
 	private const int MaxPageSize = 100;
+	private int _pageNumber = 1;
 	private int _pageSize = 10;
 
 	/// <summary>
 	/// The page number property.
 	/// </summary>
-	public int PageNumber { get; set; } = 1;
-
+	public int PageNumber
+	{
+		get => _pageNumber;
+		set => _pageNumber = value < MinPageNumber ? MinPageNumber : value;
+	}
 	/// <summary>
 	/// The page size property.
 	/// </summary>

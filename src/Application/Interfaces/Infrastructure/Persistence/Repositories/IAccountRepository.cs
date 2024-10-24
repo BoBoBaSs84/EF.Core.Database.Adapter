502ddbf3955f5cc864fc8628e57b4f2aa76c2dca
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-10-24 19:49:32 +0200

    feat: repository interfaces added and modified

--- src/Application/Interfaces/Infrastructure/Persistence/Repositories/IAccountRepository.cs
+++ src/Application/Interfaces/Infrastructure/Persistence/Repositories/IAccountRepository.cs
@@ -7,11 +7,6 @@ namespace Application.Interfaces.Infrastructure.Persistence.Repositories;
 /// <summary>
 /// The account repository interface.
 /// </summary>
-/// <remarks>
-/// Derives from the following interfaces:
-/// <list type="bullet">
-/// <item>The <see cref="IIdentityRepository{TIdentityEntity}"/> interface</item>
-/// </list>
-/// </remarks>
+/// <inheritdoc/>
 public interface IAccountRepository : IIdentityRepository<AccountModel>
 { }
--- src/Application/Interfaces/Infrastructure/Persistence/Repositories/ICardRepository.cs
+++ src/Application/Interfaces/Infrastructure/Persistence/Repositories/ICardRepository.cs
@@ -7,11 +7,6 @@ namespace Application.Interfaces.Infrastructure.Persistence.Repositories;
 /// <summary>
 /// The card repository interface.
 /// </summary>
-/// <remarks>
-/// Derives from the following interfaces:
-/// <list type="bullet">
-/// <item>The <see cref="IIdentityRepository{TIdentityEntity}"/> interface</item>
-/// </list>
-/// </remarks>
+/// <inheritdoc/>
 public interface ICardRepository : IIdentityRepository<CardModel>
 { }

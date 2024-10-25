c968d23bc252171767dcc4e00f157a5cdb9a0bf9
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-10-25 21:35:42 +0200

    feat: enumertor service tests added

---
+++ tests/ApplicationTests/Services/Common/EnumeratorServiceTests.GetCardTypes.cs
@@ -0,0 +1,48 @@
+﻿using Application.Contracts.Responses.Common;
+using Application.Errors.Services;
+using Application.Services.Common;
+
+using BaseTests.Helpers;
+
+using Domain.Errors;
+
+using FluentAssertions;
+
+namespace ApplicationTests.Services.Common;
+
+public sealed partial class EnumeratorServiceTests
+{
+	[TestMethod]
+	[TestCategory(nameof(EnumeratorService.GetCardTypes))]
+	public void GetCardTypesShouldReturnResultWhenSuccessful()
+	{
+		EnumeratorService sut = CreateMockedInstance(_mapper);
+
+		ErrorOr<IEnumerable<CardTypeResponse>> result = sut.GetCardTypes();
+
+		AssertionHelper.AssertInScope(() =>
+		{
+			result.Should().NotBeNull();
+			result.IsError.Should().BeFalse();
+			result.Errors.Count.Should().Be(0);
+			result.Value.Should().NotBeNullOrEmpty();
+		});
+	}
+
+	[TestMethod]
+	[TestCategory(nameof(EnumeratorService.GetCardTypes))]
+	public void GetCardTypesShouldReturnFailedWhenExceptionGetThrown()
+	{
+		EnumeratorService sut = CreateMockedInstance(null!);
+
+		ErrorOr<IEnumerable<CardTypeResponse>> result = sut.GetCardTypes();
+
+		AssertionHelper.AssertInScope(() =>
+		{
+			result.Should().NotBeNull();
+			result.IsError.Should().BeTrue();
+			result.Errors.First().Should().Be(EnumeratorServiceErrors.GetCardTypesFailed);
+			result.Value.Should().BeNullOrEmpty();
+		});
+	}
+}

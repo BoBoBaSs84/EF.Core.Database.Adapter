b19a8074beaa43000ce34228116cc9faba141949
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-10-28 08:50:54 +0100

    feat: document parameters validator and unit tests added

---
+++ src/Application/Validators/Features/Requests/DocumentParametersValidator.cs
@@ -0,0 +1,30 @@
+﻿using Application.Features.Requests;
+using Application.Validators.Features.Requests.Base;
+
+using FluentValidation;
+
+namespace Application.Validators.Features.Requests;
+
+/// <summary>
+/// The validator for the document parameters.
+/// </summary>
+[SuppressMessage("Style", "IDE0058", Justification = "Not relevant here, fluent validation.")]
+public sealed class DocumentParametersValidator : AbstractValidator<DocumentParameters>
+{
+	/// <summary>
+	/// Initializes an instance of <see cref="DocumentParametersValidator"/> class.
+	/// </summary>
+	public DocumentParametersValidator()
+	{
+		Include(new RequestParametersValidator());
+
+		When(x => x.Directory is not null, () => RuleFor(x => x.Directory)
+			.MaximumLength(256));
+
+		When(x => x.ExtensionName is not null, () => RuleFor(x => x.ExtensionName)
+			.MaximumLength(256));
+
+		When(x => x.Name is not null, () => RuleFor(x => x.Name)
+			.MaximumLength(256));
+	}
+}
---
+++ tests/ApplicationTests/Validators/Features/Requests/DocumentParametersValidatorTests.cs
@@ -0,0 +1,59 @@
+﻿using Application.Features.Requests;
+
+using BaseTests.Helpers;
+
+using FluentAssertions;
+
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace ApplicationTests.Validators.Features.Requests;
+
+[TestClass]
+[SuppressMessage("Style", "IDE0058", Justification = "Not relevant here, fluent assertions.")]
+public sealed class DocumentParametersValidatorTests : ApplicationTestBase
+{
+	private IValidator<DocumentParameters> _validator = default!;
+
+	[TestMethod]
+	public void ParametersShouldBeValidWhenEverythingIsCorrect()
+	{
+		_validator = CreateValidatorInstance();
+		DocumentParameters parameters = new()
+		{
+			Directory = @"C:\",
+			Name = "UnitTest",
+			ExtensionName = "txt"
+		};
+
+		ValidationResult result = _validator.Validate(parameters);
+
+		result.Should().NotBeNull();
+		result.IsValid.Should().BeTrue();
+		result.Errors.Should().BeEmpty();
+	}
+
+	[TestMethod]
+	public void ParametersShouldBeInValidWhenStringsAreTooLong()
+	{
+		_validator = CreateValidatorInstance();
+		DocumentParameters parameters = new()
+		{
+			Directory = RandomHelper.GetString(260),
+			Name = RandomHelper.GetString(260),
+			ExtensionName = RandomHelper.GetString(260)
+		};
+
+		ValidationResult result = _validator.Validate(parameters);
+
+		result.Should().NotBeNull();
+		result.IsValid.Should().BeFalse();
+		result.Errors.Should().NotBeEmpty();
+		result.Errors.Should().Contain(x => x.PropertyName == nameof(parameters.ExtensionName));
+		result.Errors.Should().Contain(x => x.PropertyName == nameof(parameters.Directory));
+		result.Errors.Should().Contain(x => x.PropertyName == nameof(parameters.Name));
+	}
+
+	private static IValidator<DocumentParameters> CreateValidatorInstance()
+		=> GetService<IValidator<DocumentParameters>>();
+}

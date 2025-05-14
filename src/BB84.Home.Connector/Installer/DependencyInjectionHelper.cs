875d8f9be8a3436e09510f60f1d517049329f55e
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2025-05-14 21:11:46 +0200

    feat: api connector started

---
+++ src/BB84.Home.Connector/Installer/DependencyInjectionHelper.cs
@@ -0,0 +1,32 @@
+﻿using BB84.Extensions;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Refit;
+
+namespace BB84.Home.Connector.Installer;
+
+/// <summary>
+/// This class is used to register the services in the dependency injection container.
+/// </summary>
+public static class DependencyInjectionHelper
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="services"></param>
+	/// <param name="baseAddress"></param>
+	/// <returns></returns>
+	public static IServiceCollection RegisterConnector(this IServiceCollection services, string baseAddress)
+	{
+		services.AddRefitClient<IBB84HomeAPI>().ConfigureHttpClient(
+			client =>
+			{
+				client.WithBaseAdress(baseAddress);
+				client.WithMediaType("application/json");
+				client.WithTimeout(TimeSpan.FromSeconds(30));
+			});
+
+		return services;
+	}
+}

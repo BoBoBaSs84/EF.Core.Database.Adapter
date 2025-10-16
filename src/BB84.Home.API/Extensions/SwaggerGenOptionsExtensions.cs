2678d2ced2c0a05546c02e48248d1973b4f1362c
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2025-10-16 14:00:00 +0200

    fix: some more small fixes applied

--- src/BB84.Home.API/Extensions/SwaggerGenOptionsExtensions.cs
+++ src/BB84.Home.API/Extensions/SwaggerGenOptionsExtensions.cs
@@ -49,12 +49,15 @@ internal static class SwaggerGenOptionsExtensions
 
 		options.CustomSchemaIds(x => x.FullName);
 
-		List<string> xmlFiles = [
-			$"{typeof(IPresentationAssemblyMarker).Assembly.GetName().Name}.xml",
-			$"{typeof(IApplicationAssemblyMarker).Assembly.GetName().Name}.xml"
-			];
+		if (environment.IsDevelopment())
+		{
+			List<string> xmlFiles = [
+				$"{typeof(IPresentationAssemblyMarker).Assembly.GetName().Name}.xml",
+				$"{typeof(IApplicationAssemblyMarker).Assembly.GetName().Name}.xml"
+				];
 
-		xmlFiles.ForEach(xml => options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xml)));
+			xmlFiles.ForEach(xml => options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xml)));
+		}
 
 		return options;
 	}
